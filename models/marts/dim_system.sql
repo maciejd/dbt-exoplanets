3dc094111f4c54953c017f8ec665107beda32116
maciejd <dobrowolski6@gmail.com>
2024-02-03 04:31:34 -0500

    refactored dim definition to CTE

--- models/marts/dim_system.sql
+++ models/marts/dim_system.sql
@@ -1,5 +1,17 @@
-select
-{{ dbt_utils.generate_surrogate_key(['hostname','sy_snum', 'sy_pnum']) }} as system_key,
-hostname, sy_snum, sy_pnum
-from {{ ref('stg_nasa__exoplanets') }}
-group by system_key, hostname, sy_snum, sy_pnum
+with stars as (
+
+    select
+        {{ dbt_utils.generate_surrogate_key(['hostname','sy_snum', 'sy_pnum', 'sy_dist', 'sy_vmag', 'sy_kmag', 'sy_gaiamag']) }} as system_key,
+        hostname, 
+        sy_snum, 
+        sy_pnum, 
+        sy_dist, 
+        sy_vmag, 
+        sy_kmag, 
+        sy_gaiamag
+
+    from {{ ref('stg_nasa__exoplanets') }}
+)
+
+select * from stars
+group by system_key, hostname, sy_snum, sy_pnum, sy_dist, sy_vmag, sy_kmag, sy_gaiamag
